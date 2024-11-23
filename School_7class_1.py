1f8e90e97e7bed60d5143bcb17f282a7e31b19ad
Alice <kalashnikova.alc@gmail.com>
2024-11-23 12:27:55 +0200

    formatter

--- School_7class_1.py
+++ School_7class_1.py
@@ -3,7 +3,8 @@ y = float(input("Ведіть значення y: "))
 x = 2*x
 answer1 = x - y
 x = 1.5*x
+
 y = 4*y
 answer2 = x + y
 answer3 = answer1 * answer2
-print (answer3)
+print(answer3)
--- School_7class_3.py
+++ School_7class_3.py
@@ -1,13 +1,14 @@
 age = int(input("Enter your age: "))
 height = int(input("Enter your height: "))
-statue = int(input( "Are you a thin-boned or broad-boned statue? : "))
+statue = int(input("Are you a thin-boned or broad-boned statue? : "))
 if age <= 40:
-  weight = height - 110
-else: 
-  weight = height - 100
+    weight = height - 110
+else:
+    weight = height - 100
+
 if statue == 1:
-  weight *= 0.9
+    weight *= 0.9
 elif statue == 2:
-  weight *= 1.1
-  
-print ("Your ideal weight is", weight)
+    weight *= 1.1
+
+print("Your ideal weight is", weight)
