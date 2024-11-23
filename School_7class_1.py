97c79906020cb7a93a19bf1c9605a6a1899fc7d1
Alice <kalashnikova.alc@gmail.com>
2024-11-23 11:56:11 +0200

    first commit

---
+++ School_7class_1.py
@@ -0,0 +1,9 @@
+x = float(input("Ведіть значення x: "))
+y = float(input("Ведіть значення y: "))
+x = 2*x
+answer1 = x - y
+x = 1.5*x
+y = 4*y
+answer2 = x + y
+answer3 = answer1 * answer2
+print (answer3)
---
+++ School_7class_2.py
@@ -0,0 +1,13 @@
+x1 = float(input("Уведіть значення змінної x1: "))
+x2 = float(input("Уведіть значення змінної x2: "))
+x3 = float(input("Уведіть значення змінної x3: "))
+x4 = float(input("Уведіть значення змінної x4: "))
+max = x1
+if max < x2:
+    max = x2
+if max < x3:
+    max = x3
+if max < x4:
+    max = x4
+print ("Найбільше число:", max)
+
